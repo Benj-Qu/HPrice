f55d92e899cfe655999327888575c505ab52287b
Benj-Qu <quzhemin@umich.com>
2023-07-31 15:14:07 +0800

    test

--- proj.py
+++ proj.py
@@ -28,19 +28,26 @@ def create_pipeline():
     preproc = ColumnTransformer(
         transformers=[
             ('log_trans', FunctionTransformer(np.log), ['Land Square Feet']),
-            ('categorical_cols', OneHotEncoder(drop='first'), categoricals())
+            (
+                'categorical_cols', 
+                OneHotEncoder(drop='first'), 
+                [
+                    'Modeling Group', 'Use', 'Wall Material', 'Roof Material', 'Garage 1 Material', 'Garage 2 Material', 
+                    'Basement', 'Basement Finish', 'Central Heating', 'Other Heating', 'Central Air', 'Attic Type', 'Attic Finish', 'Design Plan'
+                ]
+            )
         ],
         remainder='passthrough'
     )
     pipeline = Pipeline([
         ('category_encoding', FunctionTransformer(substitute_categorical_variables)),
-        ('one_hot_encoding', FunctionTransformer(one_hot_encode)),
+        # ('one_hot_encoding', FunctionTransformer(one_hot_encode)),
         ('preprocessor', preproc), 
         ('lin-reg', LinearRegression()),
     ])
     return pipeline
 
-def add_total_bedrooms(data):
+def extract_description(data):
     with_rooms = data.copy()
     with_rooms["Bedrooms"] = with_rooms["Description"].str.findall(".*(\d+) of which are bedrooms.*").str[0].fillna(0).astype(int)
     with_rooms["Rooms"] = with_rooms["Description"].str.findall(".*(\d+) rooms.*").str[0].fillna(0).astype(int)
@@ -48,8 +55,6 @@ def add_total_bedrooms(data):
     return with_rooms
 
 def one_hot_encode(data):
-    def custom_combiner(feature, category):
-        return feature + " " + str(category)
     for categorical in categoricals():
         enc = OneHotEncoder(drop='first', handle_unknown='ignore')
         enc.fit(data[[categorical]])
@@ -58,14 +63,6 @@ def one_hot_encode(data):
             index=data.index)
         data = data.join(new_cols) 
     return data
-
-def ohe_wall_material(data):
-    enc = OneHotEncoder()
-    enc.fit(data[["Wall Material"]])
-    new_cols = pd.DataFrame(enc.transform(data[["Wall Material"]]).todense(), 
-        columns = enc.get_feature_names(),
-        index = data.index)
-    return data.join(new_cols)                      
 
 def substitute_categorical_variables(data):
     categoricals = ['Apartments', 'Wall Material', 'Roof Material', 
@@ -77,4 +74,157 @@ def substitute_categorical_variables(data):
         'Porch', 'Repair Condition', 'Multi Code', 'Use']
     for categorical in categoricals:
         data[categorical] = data[categorical].astype(int)
+
+    # data = data.replace({
+    #     'Apartments': {
+    #         2: 'Two',
+    #         3: 'Three',
+    #         4: 'Four',
+    #         5: 'Five',
+    #         6: 'Six',
+    #         0: 'None',
+    #     },
+    #     'Wall Material': {
+    #         1: 'Wood',
+    #         2: 'Masonry',
+    #         3: 'Wood&Masonry',
+    #         4: 'Stucco',
+    #     },
+    #     'Roof Material': {
+    #         1: 'Shingle/Asphalt',
+    #         2: 'Tar&Gravel',
+    #         3: 'Slate',
+    #         4: 'Shake',
+    #         5: 'Tile',
+    #         6: 'Other',
+    #     },
+    #     'Basement': {
+    #         1: 'Full',
+    #         2: 'Slab',
+    #         3: 'Partial',
+    #         4: 'Crawl',
+    #     },
+    #     'Basement Finish': {
+    #         1: 'Formal rec room',
+    #         2: 'Apartment',
+    #         3: 'Unfinished',
+    #     },
+    #     'Central Heating': {
+    #         0: 'Warm air',
+    #         1: 'Hot water steam',
+    #         2: 'Electric',
+    #         3: 'Other',
+    #     },
+    #     'Other Heating': {
+    #         1: 'Floor furnace',
+    #         2: 'Unit heater',
+    #         3: 'Stove',
+    #         4: 'Solar',
+    #         5: 'none',
+    #     },
+    #     'Central Air': {
+    #         1: 'yes',
+    #         0: 'no',
+    #     },
+    #     'Attic Type': {
+    #         1: 'Full',
+    #         2: 'partial',
+    #         3: 'none',
+    #     },
+    #     'Attic Finish': {
+    #         1: 'Living area',
+    #         2: 'Apartment',
+    #         0: 'unfinished',
+    #     },
+    #     'Design Plan': {
+    #         1: 'architect',
+    #         2: 'stock plan',
+    #     },
+    #     'Cathedral Ceiling': {
+    #         1: 'yes',
+    #         2: 'no',
+    #     },
+    #     'Construction Quality': {
+    #         1: 'Deluxe',
+    #         2: 'Average',
+    #         3: 'Poor',
+    #         4: 'Other',
+    #     },
+    #     'Site Desirability': {
+    #         1: 'Beneficial to Value',
+    #         2: 'Not relevant to Value',
+    #         3: 'Detracts from Value',
+    #     },
+    #     'Garage 1 Size': {
+    #         1: '1 car',
+    #         2: '1.5 car',
+    #         3: '2 car',
+    #         4: '2.5 cars',
+    #         5: '3 cars',
+    #         6: '3.5 cars',
+    #         7: 'none',
+    #         8: '4 cars',
+    #     },
+    #     'Garage 1 Material': {
+    #         1: 'Frame',
+    #         2: 'Masonry',
+    #         3: 'Frame/Masonry',
+    #         4: 'Stucco',
+    #     },
+    #     'Garage 1 Attachment': {
+    #         1: 'Yes',
+    #         2: 'No',
+    #     },
+    #     'Garage 1 Area': {
+    #         1: 'Yes',
+    #         2: 'No',
+    #     },
+    #     'Garage 2 Size': {
+    #         1: '1 car',
+    #         2: '1.5 car',
+    #         3: '2 car',
+    #         4: '2.5 cars',
+    #         5: '3 cars',
+    #         6: '3.5 cars',
+    #         7: 'none',
+    #         8: '4 cars',
+    #     },
+    #     'Garage 2 Material': {
+    #         1: 'Frame',
+    #         2: 'Masonry',
+    #         3: 'Frame/Masonry',
+    #         4: 'Stucco',
+    #     },
+    #     'Garage 2 Attachment': {
+    #         1: 'Yes',
+    #         2: 'No',
+    #     },
+    #     'Garage 2 Area': {
+    #         1: 'Yes',
+    #         2: 'No',
+    #     },
+    #     'Porch': {
+    #         1: 'Frame',
+    #         2: 'Masonry',
+    #         3: 'None',
+    #     },
+    #     'Repair Condition': {
+    #         1: 'Above average',
+    #         2: 'Average',
+    #         3: 'Below average',
+    #     },
+    #     'Multi Code': {
+    #         2: '1 building',
+    #         3: '2 buildings',
+    #         4: '3 buildings',
+    #         5: '4 buildings',
+    #         6: '5 buildings',
+    #         7: '6 buildings',
+    #     },
+    #     'Use': {
+    #         1: 'single family',
+    #         2: 'multi-family',
+    #     },
+        
+    # })
     return data
